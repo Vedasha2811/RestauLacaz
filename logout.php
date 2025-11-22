cd2f0d3f821492d86ac16c20208a5ca12e0c1f8b
Vedasha2811 <vedashabeeputh@gmail.com>
2025-11-23 00:23:53 +0400

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+session_destroy();
+?>
+
+<html>
+    <body>
+    You are sucessfully logged out !! 
+    <a href='login.php'>Click here to log in </a>
+    </body>
+</html>
