884fe8e8b94d38f5f9725cae4a89ba536907b080
Vedasha2811 <vedashabeeputh@gmail.com>
2025-12-07 16:11:43 +0400

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -2,11 +2,13 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "restaurant_db";
+$dbname = "restaulakaz_database";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} 
+catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
